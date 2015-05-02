ef956414a4f46d396097366c07476cce96c35784
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2015-05-01 23:10:04 -0500

    Created One Hidden Layer ANN Project
    Created project to house the one hidden layer ANN project. Enabled for
    x64 and the Intel optimized compiler.

---
+++ CIFAR10Trainer/OneHiddenLayerANN/OneHiddenLayerANN.cpp
@@ -0,0 +1,13 @@
+// OneHiddenLayerANN.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
+
